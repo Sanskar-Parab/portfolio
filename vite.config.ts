1efeffb5757e042a400f82e3970ad35ea9bfb4c5
Sanskar Parab <sanskar.parab@vit.edu.in>
2026-07-18 16:39:01 +0530

    fix: change base path for vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Myportfolio/',
+  base: '/', // Change this from '/Myportfolio/' to '/'
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
